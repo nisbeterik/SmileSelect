6b5f775780eefa7b55563f0ddec1ee6af49f7ffa
Martin Lidgren <marlidg@student.chalmers.se>
2024-11-11 13:06:02 +0100

    Add directories for Vue frontend and Spring backend

---
+++ smile-select-backend/src/test/java/com/smile_select/smile_select_backend/SmileSelectBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.smile_select.smile_select_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SmileSelectBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
