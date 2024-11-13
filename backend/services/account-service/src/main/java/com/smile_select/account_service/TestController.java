3740aabb8f92731934f223977a531e811e262ca7
Erik Nisbet <eriknis@student.chalmers.se>
2024-11-13 12:36:39 +0100

    #6 Fix api URI to have /api prefix

--- backend/services/account-service/src/main/java/com/smile_select/account_service/TestController.java
+++ backend/services/account-service/src/main/java/com/smile_select/account_service/TestController.java
@@ -2,9 +2,12 @@ package com.smile_select.account_service;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.http.ResponseEntity;
 
+
 @RestController
+@RequestMapping("/api/accounts")
 public class TestController {
 
     @GetMapping("/test")
