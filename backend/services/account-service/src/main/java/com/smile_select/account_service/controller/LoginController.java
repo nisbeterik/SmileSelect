d6097f3f911e1373962604993f1e0199c5750b48
Love Strandäng <lovca@student.chalmers.se>
2024-11-13 14:58:05 +0100

    #7 Add controller folder and a login controller java file in account services

---
+++ backend/services/account-service/src/main/java/com/smile_select/account_service/controller/LoginController.java
@@ -0,0 +1,11 @@
+package main.java.com.smile_select.account_service.controller;
+
+import org.springframework.web.bind.annotation.*;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
+
+@RestController
+@RequestMapping("/api")
+public class LoginController {
+
+}
