4568de2b8195a672b82d9697910ac935ccd33347
Love Strandäng <lovca@student.chalmers.se>
2024-11-13 12:08:03 +0100

    #8 Formatting fixes from Eslint.

--- smile-select-frontend/src/components/DemoView.vue
+++ smile-select-frontend/src/components/DemoView.vue
@@ -1,30 +1,29 @@
 <template>
+  <div>
     <h1>{{ text }}</h1>
     <button @click="helloWorld">Click Here</button>
+  </div>
 </template>
 
 <script>
-
-
 export default {
-    data() {
-        return {
-            text: ""
-        }
+  data() {
+    return {
+      text: '',
+    };
+  },
+  methods: {
+    async helloWorld() {
+      try {
+        const response = await this.$axios.get('/helloworld');
+        this.text = response.data;
+      } catch (error) {
+        console.error('Error fetching data:', error);
+        this.text = 'Error occurred while fetching data';
+      }
     },
-    methods: {
-        async helloWorld() {
-            try {
-                const response = await this.$axios.get('/helloworld');
-                this.text = response.data;
-            } catch (error) {
-                console.error('Error fetching data:', error);
-                this.text = 'Error occurred while fetching data';
-            }
-
-        }
-    }
-}
+  },
+};
 </script>
 
 <style scoped></style>
