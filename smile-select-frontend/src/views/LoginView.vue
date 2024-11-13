797eec40fada02c9545b414f8b9b701fd4aaecce
Love Strandäng <lovca@student.chalmers.se>
2024-11-13 14:22:43 +0100

    #7 Create login view file and ensure proper routing.

---
+++ smile-select-frontend/src/views/LoginView.vue
@@ -0,0 +1,13 @@
+<template>
+    <div>
+      <h1>Login Page</h1>
+      <p>This is the login page.</p>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'LoginView',
+  };
+  </script>
+  
