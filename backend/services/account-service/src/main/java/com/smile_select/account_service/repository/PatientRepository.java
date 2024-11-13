e51cca5bca455358cd64730af8dd6fbcae4a3b86
Love Strandäng <lovca@student.chalmers.se>
2024-11-13 21:08:35 +0100

    #7 Add PatientRepository for accessing patient data by email

---
+++ backend/services/account-service/src/main/java/com/smile_select/account_service/repository/PatientRepository.java
@@ -0,0 +1,11 @@
+package main.java.com.smile_select.account_service.repository;
+
+import com.smile_select.account_service.model.Patient;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+import java.util.Optional;
+
+@Repository
+public interface PatientRepository extends JpaRepository<Patient, Long> {
+    Optional<Patient> findByEmail(String email);
+}
