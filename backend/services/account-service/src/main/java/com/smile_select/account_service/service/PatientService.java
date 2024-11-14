b27b9cc65947c0c4bd90f86a747b2a36ae55a1d0
Love Strandäng <lovca@student.chalmers.se>
2024-11-14 12:37:30 +0100

    #7 Add initial logic for patient service

--- backend/services/account-service/src/main/java/com/smile_select/account_service/service/PatientService.java
+++ backend/services/account-service/src/main/java/com/smile_select/account_service/service/PatientService.java
@@ -1,5 +1,22 @@
-package main.java.com.smile_select.account_service.service;
+package com.smile_select.account_service.service;
 
+import com.smile_select.account_service.model.Patient;
+import com.smile_select.account_service.repository.PatientRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.Optional;
+
+@Service
 public class PatientService {
-    
+
+    @Autowired
+    private PatientRepository patientRepository;
+
+    public Optional<Patient> findPatientByEmail(String email) {
+        return patientRepository.findByEmail(email);
+    }
+
+    public Patient savePatient(Patient patient) {
+        return patientRepository.save(patient);
+    }
 }
