e4522bdf920856597802c3d6a1638cab9a393d4f
Martin Lidgren <marlidg@student.chalmers.se>
2024-11-14 11:58:36 +0100

    #9 Add controller and service class for Appointment, with POST-method for creating appointments

---
+++ backend/services/appointment-service/src/main/java/com/smile_select/appointment_service/controller/AppointmentController.java
@@ -0,0 +1,35 @@
+package com.smile_select.appointment_service.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.smile_select.appointment_service.model.Appointment;
+import com.smile_select.appointment_service.service.AppointmentService;
+
+
+@RestController
+@RequestMapping("/api/appointments") 
+public class AppointmentController {
+
+    private final AppointmentService appointmentService;
+    
+    @Autowired
+    public AppointmentController(AppointmentService appointmentService) {
+        this.appointmentService = appointmentService;
+    }
+
+    @PostMapping
+    public ResponseEntity<?> createAppointment(@RequestBody Appointment appointment) {
+        if (appointment.getDentistId() == 0) {
+            return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("Dentist ID is required.");
+        }
+        Appointment createdAppointment = appointmentService.createAppointment(appointment);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdAppointment);
+}
+    
+}
