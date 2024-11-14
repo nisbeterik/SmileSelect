0c809a53cda467db85b964246f735f62fb39f481
Love Strandäng <lovca@student.chalmers.se>
2024-11-14 13:18:49 +0100

    #7 Update index.js to support dentist and patient dashboard routes.

--- smile-select-frontend/src/router/index.js
+++ smile-select-frontend/src/router/index.js
@@ -1,7 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import LoginView from '@/views/LoginView.vue';
+import PatientDashboard from '@/views/PatientDashboard.vue';
+import DentistDashboard from '@/views/DentistDashboard.vue';
 
-const routes = [{ path: '/login', name: 'Login', component: LoginView }];
+const routes = [
+  { path: '/login', name: 'Login', component: LoginView },
+  {
+    path: '/patient-dashboard',
+    name: 'PatientDashboard',
+    component: PatientDashboard,
+  },
+  {
+    path: '/dentist-dashboard',
+    name: 'DentistDashboard',
+    component: DentistDashboard,
+  },
+  // Add other routes as needed
+];
 
 const router = createRouter({
   history: createWebHistory(),
