f37f3a6d2b3238668f1980149bcdf81c5cd93091
Love Strandäng <lovca@student.chalmers.se>
2024-11-14 11:39:52 +0100

    #7 Add logic for dentist login

--- backend/services/account-service/src/main/java/com/smile_select/account_service/controller/LoginController.java
+++ backend/services/account-service/src/main/java/com/smile_select/account_service/controller/LoginController.java
@@ -14,15 +14,22 @@ import java.util.Map;
 import java.util.Optional;
 
 @RestController
-@RequestMapping("/api")
+@RequestMapping("/api/login")
 public class LoginController {
 
-    @PostMapping("/login")
-    public ResponseEntity<?> login(@RequestBody LoginRequest LoginRequest){
-        //Placeholder logic
-        if("placeholderuser@mail.com".equals(loginRequest.getEmail())){
+    @Autowired
+    private DentistService dentistService;
 
+    @PostMaping("/dentist")
+    public ReponseEntity<?> loginDentist(@RequestBody Map<String, String> credentials) {
+        String email = credentials.get("email");
+        String password = credentials.get("password");
+
+        Optional<main.java.com.smile_select.account_service.model.Dentist> dentist = dentistService
+                .findDentistByEmail(email);
+        if (dentist.isPresent() && dentist.get().getPassword().equals(password)) {
+            return ResponseEntity.ok("Login Successful");
         }
+        return ReponseEntity.status(HttpStatus.UNAUTHORIZED).body("Invalid credentials");
     }
-
 }
--- backend/services/account-service/src/main/java/com/smile_select/account_service/service/DentistService.java
+++ backend/services/account-service/src/main/java/com/smile_select/account_service/service/DentistService.java
@@ -1,5 +1,17 @@
 package main.java.com.smile_select.account_service.service;
 
+import main.java.com.smile_select.account_service.repository.DentistRepository;
+
 public class DentistService {
-    
+
+    @Autowired
+    private DentistRepository dentistRepository;
+
+    public Optional<Dentist> findDentistByEmail(String email){
+        return dentistRepository.findDentistByEmail(email);
+    }
+
+    public Dentist saveDentist(Dentist dentist){
+        return dentistRepository.save(dentist);
+    }
 }
