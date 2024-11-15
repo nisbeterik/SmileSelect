d63f55675bf6d40e0e588345538609f94604bbe6
Martin Lidgren <marlidg@student.chalmers.se>
2024-11-15 14:35:55 +0100

    #13 Add endpoint for getting all appointments

--- backend/services/appointment-service/src/main/java/com/smile_select/appointment_service/controller/AppointmentController.java
+++ backend/services/appointment-service/src/main/java/com/smile_select/appointment_service/controller/AppointmentController.java
@@ -1,8 +1,11 @@
 package com.smile_select.appointment_service.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,13 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 import com.smile_select.appointment_service.model.Appointment;
 import com.smile_select.appointment_service.service.AppointmentService;
 
-
 @RestController
-@RequestMapping("/api/appointments") 
+@RequestMapping("/api/appointments")
 public class AppointmentController {
 
     private final AppointmentService appointmentService;
-    
+
     @Autowired
     public AppointmentController(AppointmentService appointmentService) {
         this.appointmentService = appointmentService;
@@ -30,6 +32,12 @@ public class AppointmentController {
         }
         Appointment createdAppointment = appointmentService.createAppointment(appointment);
         return ResponseEntity.status(HttpStatus.CREATED).body(createdAppointment);
-}
-    
+    }
+
+    @GetMapping
+    public ResponseEntity<?> getAllAppointments(){
+        List <Appointment> appointments = appointmentService.getAllAppointments();
+        return ResponseEntity.status(HttpStatus.OK).body(appointments);
+    }
+
 }
