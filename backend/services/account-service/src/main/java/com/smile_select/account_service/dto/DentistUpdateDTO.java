f29ae0aa7f390fa4bffff8169f207b029fcda99e
fnilsson <gusnilfral@student.gu.se>
2024-11-20 10:27:33 +0100

    #15 - Update DentistDTO and DentistUpdateDTO

--- backend/services/account-service/src/main/java/com/smile_select/account_service/dto/DentistUpdateDTO.java
+++ backend/services/account-service/src/main/java/com/smile_select/account_service/dto/DentistUpdateDTO.java
@@ -1,4 +1,26 @@
 package com.smile_select.account_service.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+
+/**
+ * A Data Transfer Object (DTO) for updating Dentist entity data.
+ */
+@Setter
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
 public class DentistUpdateDTO {
+    private String firstName;
+    private String lastName;
+    private String email;
+    private String password;
+    private double longitude;
+    private double latitude;
+    private String street;
+    private int zip;
+    private String city;
 }
