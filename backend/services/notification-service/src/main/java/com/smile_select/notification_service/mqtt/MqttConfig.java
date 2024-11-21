cb2d2334c51ea65c18cbe721de441f10fbe462b9
Martin Lidgren <marlidg@student.chalmers.se>
2024-11-21 15:07:03 +0100

    #20 Add MQTT functionality to Notification service

---
+++ backend/services/notification-service/src/main/java/com/smile_select/notification_service/mqtt/MqttConfig.java
@@ -0,0 +1,92 @@
+
+package com.smile_select.notification_service.mqtt;
+
+import org.eclipse.paho.client.mqttv3.MqttConnectOptions;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.integration.annotation.ServiceActivator;
+import org.springframework.integration.channel.DirectChannel;
+import org.springframework.integration.core.MessageProducer;
+import org.springframework.integration.mqtt.core.DefaultMqttPahoClientFactory;
+import org.springframework.integration.mqtt.core.MqttPahoClientFactory;
+import org.springframework.integration.mqtt.inbound.MqttPahoMessageDrivenChannelAdapter;
+import org.springframework.integration.mqtt.outbound.MqttPahoMessageHandler;
+import org.springframework.integration.mqtt.support.DefaultPahoMessageConverter;
+import org.springframework.integration.mqtt.support.MqttHeaders;
+import org.springframework.messaging.Message;
+import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.MessageHandler;
+import org.springframework.messaging.MessagingException;
+import java.util.UUID;
+
+@Configuration
+public class MqttConfig {
+
+    @Bean
+    public MqttPahoClientFactory mqttClientFactory() {
+        DefaultMqttPahoClientFactory factory = new DefaultMqttPahoClientFactory();
+        MqttConnectOptions options = new MqttConnectOptions();
+
+        options.setServerURIs(new String[] { "tcp://localhost:1883" });
+        options.setCleanSession(false);
+        options.setKeepAliveInterval(60);
+        options.setConnectionTimeout(30);
+        options.setAutomaticReconnect(true);
+
+        factory.setConnectionOptions(options);
+        return factory;
+    }
+
+    @Bean
+    public MessageChannel mqttInputChannel() {
+        return new DirectChannel();
+    }
+    
+    @Bean
+    public MessageProducer inbound() {
+        String clientId = "serverIn-" + UUID.randomUUID().toString();
+        MqttPahoMessageDrivenChannelAdapter adapter = new MqttPahoMessageDrivenChannelAdapter(clientId,
+                mqttClientFactory(), "#");
+
+        adapter.setCompletionTimeout(5000);
+        adapter.setConverter(new DefaultPahoMessageConverter());
+        adapter.setQos(2);
+        adapter.setOutputChannel(mqttInputChannel());
+        return adapter;
+    }
+    
+    @Bean
+    @ServiceActivator(inputChannel = "mqttInputChannel")
+    public MessageHandler handler() {
+        return new MessageHandler() {
+            @Override
+            public void handleMessage(Message<?> message) throws MessagingException {
+                String topic = message.getHeaders().get(MqttHeaders.RECEIVED_TOPIC).toString();
+
+                // Handle new appointment slot created
+                if(topic.equals("/appointments/new")) {
+                    // Placeholder actions
+                    System.out.println("Received message from topic: " + topic);
+                    System.out.println("Payload: " + message.getPayload());
+                }
+				// Handle more topics below...
+            }
+        };
+    }
+    
+    @Bean
+    public MessageChannel mqttOutboundChannel() {
+        return new DirectChannel();
+    }
+
+    @Bean
+    @ServiceActivator(inputChannel = "mqttOutboundChannel")
+    public MessageHandler mqttOutbound() {
+        String clientId = "serverOut-" + UUID.randomUUID().toString();
+        MqttPahoMessageHandler messageHandler = new MqttPahoMessageHandler(clientId, mqttClientFactory());
+        messageHandler.setAsync(true);
+        messageHandler.setDefaultTopic("/notifications");
+        messageHandler.setDefaultRetained(false);
+        return messageHandler;
+    }
+}
