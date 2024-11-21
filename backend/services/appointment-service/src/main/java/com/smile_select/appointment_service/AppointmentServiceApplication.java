b3da37fd20cb14408981584d28d01ebbe60be55d
Martin Lidgren <marlidg@student.chalmers.se>
2024-11-21 12:32:24 +0100

    #20 Add component scan to application to fix compilation issue

--- backend/services/appointment-service/src/main/java/com/smile_select/appointment_service/AppointmentServiceApplication.java
+++ backend/services/appointment-service/src/main/java/com/smile_select/appointment_service/AppointmentServiceApplication.java
@@ -2,8 +2,10 @@ package com.smile_select.appointment_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
+@ComponentScan(basePackages = {"com.smile_select"})
 public class AppointmentServiceApplication {
 
 	public static void main(String[] args) {
