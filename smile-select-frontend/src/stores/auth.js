9aabfe17806e23a73689e82614adfbd29f42df60
Love Strandäng <lovca@student.chalmers.se>
2024-12-03 13:05:43 +0100

    #39 update auth store to include methods such as setting a role and id, and clearing the cache.

--- smile-select-frontend/src/stores/auth.js
+++ smile-select-frontend/src/stores/auth.js
@@ -3,15 +3,29 @@ import { defineStore } from 'pinia';
 export const useAuthStore = defineStore('auth', {
   state: () => ({
     token: localStorage.getItem('jwtToken') || '',
+    role: localStorage.getItem('userRole') || '',
+    id: localStorage.getItem('userId') || '',
   }),
   actions: {
     setToken(token) {
       this.token = token;
       localStorage.setItem('jwtToken', token);
     },
-    clearToken() {
+    setRole(role) {
+      this.role = role;
+      localStorage.setItem('userRole', role);
+    },
+    setId(id) {
+      this.id = id;
+      localStorage.setItem('userId', id);
+    },
+    clearAuth() {
       this.token = '';
+      this.role = '';
+      this.id = '';
       localStorage.removeItem('jwtToken');
+      localStorage.removeItem('userRole');
+      localStorage.removeItem('userId');
     },
   },
   getters: {
