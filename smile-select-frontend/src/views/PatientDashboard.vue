ff7cf04b6157c5675a908b9399c442036fc33e7a
Martin Lidgren <marlidg@student.chalmers.se>
2024-12-04 16:39:35 +0100

    #41 Update patient dashboard to include new component

--- smile-select-frontend/src/views/PatientDashboard.vue
+++ smile-select-frontend/src/views/PatientDashboard.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="dashboard">
-    <h1>Welcome to the Patient Dashboard!</h1>
+    <div class="text-center mb-4">
+      <h1 class="header-title">Welcome to the Patient Dashboard!</h1>
+    </div>
+    <PatientCurrentAppointment />
   </div>
 </template>
 
 <script>
+import PatientCurrentAppointment from '@/views/PatientCurrentAppointment.vue';
+
 export default {
   name: 'PatientDashboard',
+  components: {
+    PatientCurrentAppointment, 
+  },
 };
 </script>
+
+<style scoped>
+.header-title {
+  padding: 20px;
+}
+</style>
