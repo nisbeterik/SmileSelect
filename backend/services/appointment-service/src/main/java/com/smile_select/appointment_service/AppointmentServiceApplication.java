e763682dd367309648fe04c9364a68b760af26b9
fnilsson <gusnilfral@student.gu.se>
2024-12-06 18:47:00 +0100

    #44 - Implement Cleanup functionality
    Cleanup functionality is scheduled to run every hour and remove appointments older than 96 hours in the redundant database

--- backend/services/appointment-service/src/main/java/com/smile_select/appointment_service/AppointmentServiceApplication.java
+++ backend/services/appointment-service/src/main/java/com/smile_select/appointment_service/AppointmentServiceApplication.java
@@ -3,8 +3,10 @@ package com.smile_select.appointment_service;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
+@EnableScheduling
 @ComponentScan(basePackages = {"com.smile_select"})
 public class AppointmentServiceApplication {
 
--- backend/services/appointment-service/src/main/java/com/smile_select/appointment_service/config/DataSourceConfig.java
+++ backend/services/appointment-service/src/main/java/com/smile_select/appointment_service/config/DataSourceConfig.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.jdbc.DataSourceBuilder;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Primary;
 import org.springframework.jdbc.core.JdbcTemplate;
 
 @Configuration
@@ -31,6 +32,7 @@ public class DataSourceConfig {
     private String fallbackPassword;
 
     @Bean(name = "primaryDataSource")
+    @Primary
     public DataSource primaryDataSource() {
         return DataSourceBuilder.create()
                 .url(primaryUrl)
