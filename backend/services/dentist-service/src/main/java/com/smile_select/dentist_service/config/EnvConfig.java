f875e39476aa1ae759016196dccd2657c53cb84a
Erik Nisbet <eriknis@student.chalmers.se>
2024-12-06 04:38:11 +0100

    #43 Remove profile ci from several files

--- backend/services/dentist-service/src/main/java/com/smile_select/dentist_service/config/EnvConfig.java
+++ backend/services/dentist-service/src/main/java/com/smile_select/dentist_service/config/EnvConfig.java
@@ -1,9 +1,11 @@
 package com.smile_select.dentist_service.config;
 
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.context.annotation.PropertySource;
 
 @Configuration
+@Profile("!ci")
 @PropertySource("classpath:env.properties") // Loads the env.properties file from the classpath
 public class EnvConfig {
 }
