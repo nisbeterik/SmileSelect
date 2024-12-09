94ffda1e9863493405ccda5d54b7fb8ed5de6fcf
fnilsson <gusnilfral@student.gu.se>
2024-12-09 11:10:13 +0100

    #47 - Create Clinic files for further implementation

---
+++ backend/services/dentist-service/src/main/java/com/smile_select/dentist_service/dto/ClinicUpdateDTO.java
@@ -0,0 +1,4 @@
+package com.smile_select.dentist_service.dto;
+
+public class ClinicUpdateDTO {
+}
---
+++ backend/services/dentist-service/src/main/java/com/smile_select/dentist_service/service/ClinicService.java
@@ -0,0 +1,4 @@
+package com.smile_select.dentist_service.service;
+
+public class ClinicService {
+}
