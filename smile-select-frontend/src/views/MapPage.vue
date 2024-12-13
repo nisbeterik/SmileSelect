e7e88d15029eaebf295d20aef9422d774771d724
Erik Nisbet <eriknis@student.chalmers.se>
2024-12-13 14:29:24 +0100

    #58 Fix map in MapPage.vue, all locations are loaded properly

--- smile-select-frontend/src/views/MapPage.vue
+++ smile-select-frontend/src/views/MapPage.vue
@@ -1,24 +1,36 @@
 <template>
   <LMap
-      :zoom="13"
+      :zoom="12"
       :center="[57.7089, 11.9746]"
       :options="mapOptions"
       style="height: 1080px; width: 100%;"
   >
-    <LTileLayer :url="'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'" />
+    <LTileLayer :url="'https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png'"/>
+
     <LMarker
         v-for="clinic in clinics"
         :key="clinic.id"
         :lat-lng="[clinic.latitude, clinic.longitude]"
+        :visible="true"
     />
 
-    <l-control-scale position="topright" />
+    <LMarker
+        :lat-lng="[57.7075, 11.97]"
+        :visible="true"
+    />
+
+    <LMarker
+        :lat-lng="[57.683, 11.959]"
+        :visible="true"
+    />
+
+    <l-control-scale position="topright"/>
   </LMap>
 </template>
 
 <script>
 import "leaflet/dist/leaflet.css";
-import { LControlScale, LMap, LTileLayer, LMarker } from "@vue-leaflet/vue-leaflet";
+import {LControlScale, LMap, LTileLayer, LMarker} from "@vue-leaflet/vue-leaflet";
 import axios from "@/axios";
 
 export default {
@@ -33,12 +45,12 @@ export default {
       mapOptions: {
         maxBounds: [
           [57.65, 11.85],
-          [57.75, 12.05],
+          [57.76, 12.1]
         ],
         maxBoundsViscosity: 1.0,
         minZoom: 12,
       },
-      clinics: []
+      clinics: [],
     };
   },
   created() {
@@ -47,31 +59,12 @@ export default {
   methods: {
     async fetchClinics() {
       try {
-
         const response = await axios.get('/dentists/clinics');
         this.clinics = response.data;
       } catch (error) {
         console.error("Error fetching clinics:", error);
       }
     }
-  }
+  },
 };
 </script>
-
-<style>
-body, html, #app {
-  margin: 0;
-  padding: 0;
-  height: 100%;
-}
-
-.l-map-container {
-  height: 100vh;
-  width: 100vw;
-}
-
-
-.leaflet-control-attribution {
-  display: none !important;
-}
-</style>
