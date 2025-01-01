cde98c6e14f6adb508d91cea3456a330d061b239
Erik Nisbet <eriknis@student.chalmers.se>
2025-01-01 19:02:57 +0100

    #68 Add settings in application.properties and correct annotations for main class

--- backend/registry/service-registry/src/main/java/com/smile_select/service_registry/ServiceRegistryApplication.java
+++ backend/registry/service-registry/src/main/java/com/smile_select/service_registry/ServiceRegistryApplication.java
@@ -2,8 +2,11 @@ package com.smile_select.service_registry;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
 
 @SpringBootApplication
+@EnableEurekaServer
 public class ServiceRegistryApplication {
 
 	public static void main(String[] args) {
