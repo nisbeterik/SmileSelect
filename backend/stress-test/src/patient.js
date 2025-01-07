387a4bc062953b497aa3d43ff990d006289cc773
Erik Nisbet <eriknis@student.chalmers.se>
2025-01-07 13:43:55 +0100

    #75 Add light and full stress-test in package.json

--- backend/stress-test/src/patient.js
+++ backend/stress-test/src/patient.js
@@ -1,14 +1,22 @@
 import http from 'k6/http';
 import { check, sleep } from 'k6';
-import { uuidv4 } from './helpers.js'; // Optional, if you create a UUID helper
+import { uuidv4 } from './helpers.js';
 
+const fullStages = [
+    { duration: '1m', target: 3000 }, // Ramp up to 3000 users in 1 minute
+    { duration: '1m', target: 3000 }, // Maintain 3000 users for 1 minutes
+    { duration: '1m', target: 0 },   // Ramp down to 0 users in 1 minute
+];
+
+const lightStages = [
+    { duration: '10s', target: 10 }, // Ramp up to 10 users in 10 seconds
+    { duration: '10s', target: 10 }, // Maintain 10 users for 10 seconds
+    { duration: '10s', target: 0 },  // Ramp down to 0 users in 10 seconds
+];
+
+// Dynamically set stages based on the environment variable
 export const options = {
-    stages: [
-        { duration: '1m', target: 1000 },
-        { duration: '1m', target: 3000 },
-        { duration: '1m', target: 4000 },
-        { duration: '30s', target: 0 }
-    ]
+    stages: __ENV.RUN_MODE === 'full' ? fullStages : lightStages,
 };
 
 function registerPatient() {
